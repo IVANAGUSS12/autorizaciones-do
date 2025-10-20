e9658b3ac0886870c779d312cf20e73975f28daf
unknown <iperez@cemic1.edu.ar>
2025-10-20 14:18:43 -0300

    Always run makemigrations+migrate on boot

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,10 @@
 set -e
 export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-autorizaciones.settings}
 mkdir -p /workspace /workspace/staticfiles /workspace/data/media
+
+# SIEMPRE preparar DB
+python -m django makemigrations --noinput || true
 python -m django migrate --noinput
+
 python -m django collectstatic --noinput || true
 exec gunicorn autorizaciones.wsgi:application --bind 0.0.0.0:${PORT:-8080} --workers ${GUNICORN_WORKERS:-3} --timeout 120
