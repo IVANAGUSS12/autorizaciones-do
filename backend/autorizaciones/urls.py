24bfc515a93ed4c5fb1ac6c2319f16dabecae056
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-20 23:27:25 -0300

    Update urls.py

--- backend/autorizaciones/urls.py
+++ backend/autorizaciones/urls.py
@@ -1,8 +1,14 @@
+from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    # Admin de Django
+    path("admin/", admin.site.urls),
+
+    # API + vistas del core
     path("v1/", include("core.urls")),
-    # Redirige raíz al panel
-    path("", RedirectView.as_view(url="/static/panel/index.html", permanent=False)),
+
+    # Raíz -> panel
+    path("", RedirectView.as_view(url="/v1/panel/", permanent=False)),
 ]
