2dbaba76f9962312a06b82fd8d12ff1d6e0a486f
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-20 22:22:45 -0300

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-
 from .views import PatientViewSet, AttachmentViewSet
 from .views_media import media_signed
-
 
 router = DefaultRouter()
 router.register(r"patients", PatientViewSet, basename="patients")
 router.register(r"attachments", AttachmentViewSet, basename="attachments")
 
-
 urlpatterns = [
-path("", include(router.urls)),
-path("media-signed/<path:object_key>", media_signed, name="media_signed"),
+    path("", include(router.urls)),
+    path("media-signed/<path:object_key>", media_signed, name="media_signed"),
 ]
