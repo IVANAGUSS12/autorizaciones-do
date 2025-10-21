eb4d4e85304f8f81bf3d6ec01c3bd6412ca694ab
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-21 14:01:00 -0300

    Update views_health.py

--- backend/core/views_health.py
+++ backend/core/views_health.py
@@ -1,8 +1,4 @@
 from django.http import JsonResponse
 
 def health(request):
-    """
-    Endpoint simple para los health checks de DigitalOcean.
-    Devuelve siempre 200 OK si el servidor está corriendo.
-    """
     return JsonResponse({"status": "ok"})
