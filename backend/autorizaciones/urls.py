6b28f5f5f75107bbe0eca6d36d6d70c811713df2
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-20 22:54:01 -0300

    Update urls.py

--- backend/autorizaciones/urls.py
+++ backend/autorizaciones/urls.py
@@ -1,24 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 from django.views.generic import RedirectView
-from django.views.static import serve as static_serve
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("v1/", include("core.urls")),
-
-    # Fronts
+    # Redirección de la raíz al panel estático
     path("", RedirectView.as_view(url="/static/panel/index.html", permanent=False)),
-    path("static/qr/", RedirectView.as_view(url="/static/qr/index.html", permanent=False)),
 ]
-
-# Servir MEDIA también en producción (App Platform no pone Nginx)
-urlpatterns += [
-    re_path(r"^media/(?P<path>.*)$", static_serve, {"document_root": settings.MEDIA_ROOT}),
-]
-
-# Y en desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
