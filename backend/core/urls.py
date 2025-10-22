5856153823441c52914e2ee427621b00525bf290
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-22 18:57:55 -0300

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import PatientViewSet, AttachmentViewSet
+from .views import PatientViewSet, AttachmentViewSet, health
 
 router = DefaultRouter()
 router.register(r"patients", PatientViewSet, basename="patients")
 router.register(r"attachments", AttachmentViewSet, basename="attachments")
 
 urlpatterns = [
+    # /v1/health/
+    path("health/", health, name="health"),
+
+    # /v1/patients/  /v1/attachments/
     path("", include(router.urls)),
-    # si ya tenés /v1/health/ en otro archivo, dejalo como estaba
 ]
