c8d113b4c1b26d1a483320c9ca355f1e77c0ff7c
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-22 21:00:59 -0300

    Update urls.py

--- backend/autorizaciones/urls.py
+++ backend/autorizaciones/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-# (opcional) si querés servir /panel/ y /qr/:
-# from core.views_panel import panel_index, qr_index
+from core.views_panel import panel_index, qr_index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("v1/", include("core.urls")),
-    # Opcional:
-    # path("panel/", panel_index, name="panel"),
-    # path("qr/", qr_index, name="qr"),
+    path("panel/", panel_index, name="panel"),
+    path("qr/", qr_index, name="qr"),
 ]
