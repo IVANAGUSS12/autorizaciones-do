2ba52af4cfe6b14218868d154595c3b8654676cd
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-22 22:28:41 -0300

    Create templates.py

---
+++ backend/core/templates.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        # Incluimos rutas explícitas; APP_DIRS=True también ayuda.
+        "DIRS": [BASE_DIR / "core" / "templates"],  # <- clave para /panel y /qr
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
